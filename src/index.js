7d4659f6d8f64c2c318425865ad6ee63edc117a1
David-Sarif <DavidSarif@yandex.ru>
2020-02-27 06:05:42 +0300

    feat:solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+
+    if (n < 0) {
+
+        return (+String(n).slice(1).split('').reverse().join(''))
+
+    }
+  return +String(n).split('').reverse().join('')
 }
